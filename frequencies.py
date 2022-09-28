24eaa5c1655451f24f5058ff36387c73ba5b280e
Oliver Macpherson <oliver.macpherson@kcl.ac.uk>
2022-09-28 13:59:07 +0100

    Add functionality to counting instances in frequencies dict

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,16 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    count = len(items)
+    for i in range(count):
+        if str(items[i]) in frequencies.keys(): #or str(items[i]) in frequencies.keys():
+            frequencies.update({str(items[i]):frequencies.get(str(items[i]))+1})
+        else:
+            #frequencies.add({items[i]:1})
+            frequencies[str(items[i])] = 1
+
     return frequencies
+
+
+
+#print(frequencies((100, 'Hello', '100', '100', 100)))
